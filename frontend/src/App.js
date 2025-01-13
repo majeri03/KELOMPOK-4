493a9271d1413cbbfd981ccac9982f27cdc0ff48
majeri03 <105841103622@student.unismuh.ac.id>
2025-01-13 11:48:26 +0800

    2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-
+import Login from "./components/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Register from "./components/Register";
 function App() {
   return (
-    <div >
-      <h1>Hello world</h1>
-    </div>
+    <BrowserRouter >
+    <Routes>
+    <Route path="/" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
